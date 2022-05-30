4902173ce9e6a8a20432f7d837acf7c42e815b3b
Maxim Khvatalin <khmm12@gmail.com>
2022-05-30 16:57:33 +0300

    feat: format percent values via Intl API

--- src/components/TimeMilestones/components/Milestone/index.tsx
+++ src/components/TimeMilestones/components/Milestone/index.tsx
@@ -1,4 +1,6 @@
 import type { JSX } from 'solid-js'
+import round from '@/utils/round'
+import createIntlFormatter from '@/hooks/createIntlFormatter'
 import Bar from './components/Bar'
 import * as css from './styles'
 
@@ -7,9 +9,14 @@ interface MilestoneProps {
   description: JSX.Element
 }
 
-const formatValue = (value: number): string => `${Math.floor(value * 100)}%`
+const PercentFormatOptions = { style: 'percent', minimumFractionDigits: 0, maximumFractionDigits: 0 }
 
 export default function Milestone(props: MilestoneProps): JSX.Element {
+  const intl = createIntlFormatter()
+
+  const formatPercent = (value: number): string => intl.number(value, PercentFormatOptions)
+  const formatValue = (value: number): string => formatPercent(round(value, 2)).replaceAll(/\s/g, '')
+
   return (
     <div class={css.container}>
       <span class={css.value}>{formatValue(props.value)}</span>
