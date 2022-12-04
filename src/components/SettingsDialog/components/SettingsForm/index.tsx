3520163eabc444bd8024efa9cf258e0e78bf13df
Maxim Khvatalin <khmm12@gmail.com>
2022-12-04 15:41:29 +0300

    chore: drop useless hook

--- src/components/SettingsDialog/components/SettingsForm/index.tsx
+++ src/components/SettingsDialog/components/SettingsForm/index.tsx
@@ -1,4 +1,4 @@
-import { createComputed, createEffect, JSX, on } from 'solid-js'
+import { createEffect, JSX, on } from 'solid-js'
 import { createForm } from '@felte/solid'
 import toISODate from '@/utils/to-iso-date'
 import createUniqueIds from '@/hooks/createUniqueIds'
@@ -11,13 +11,12 @@ interface SettingsFormProps {
 }
 
 export default function SettingsForm(props: SettingsFormProps): JSX.Element {
-  const { form, isSubmitting, setInitialValues, isDirty, reset } = createForm<Settings>({
+  const { form, isSubmitting, isDirty, setInitialValues, reset } = createForm<Settings>({
     initialValues: props.initialValues,
     transform: (values) => transformValues(values as Settings),
     onSubmit: (values) => props.onSubmit(values),
   })
 
-  createComputed(() => props.initialValues)
   createEffect(
     on(
       () => props.initialValues,
