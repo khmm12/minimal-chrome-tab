ee43fb3407c6db09fcc1e9402ea253e2768f8764
Maxim Khvatalin <khmm12@gmail.com>
2022-12-04 15:35:50 +0300

    fix: chrome detection

--- src/utils/storage/utils.ts
+++ src/utils/storage/utils.ts
@@ -1,7 +1,8 @@
 import type { IStorageAdapterConstructor } from './types'
 
 export async function getStorageAdapter<T>(): Promise<IStorageAdapterConstructor<T>> {
-  if (typeof chrome !== 'undefined') return (await import('./adapters/chrome-storage-adapter')).default
+  if (typeof chrome !== 'undefined' && typeof chrome.storage !== 'undefined')
+    return (await import('./adapters/chrome-storage-adapter')).default
   if (typeof localStorage !== 'undefined') return (await import('./adapters/local-starage-adapter')).default
   return (await import('./adapters/memory-storage-adapter')).default
 }
